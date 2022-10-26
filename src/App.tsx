9b3f76e9ac7893aad0d1f6c7493e615d84504a09
jsPark <wotjddnfl@gmail.com>
2022-10-27 02:28:52 +0900

    fix:

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-// import CreateBox from './components/CreateTag';
-import TagMain from './components/TagMain';
-// import SelectTag from './components/SelectTag';
 
 function App() {
-  return (
-    <div className="App">
-      <div
-        style={{
-          backgroundColor: 'skyblue',
-          height: '844px',
-          width: '390px',
-        }}
-      > 
-        <div>Area</div>
-        <TagMain />
-      </div>
-    </div>
-  );
-
-
+  return <div className='App'></div>;
+}
 export default App;
